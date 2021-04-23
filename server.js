848e88a366d8bb567742172bcdd007f1698cae18
mesolly <saurav.kandari.123@gmail.com>
2021-04-23 13:37:40 +0530

    'HomePage'

--- server.js
+++ server.js
@@ -7,13 +7,19 @@ const app = express() ;
 
 const PORT=process.env.PORT || 3000 ;
 
-app.get('/',(req,res)=>{
-    res.render('home') ;
-});
-
 app.use(expressLayout) ;
 app.set('views',path.join(__dirname,'resources/views'));
 app.set('view engine','ejs');
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>{
+    res.render('home') ;
+});
+app.get('/cart',(req,res)=>{
+    res.render('customers/cart');
+});
+
+
 
 app.listen(PORT,()=>{
     console.log(`Listening on port ${PORT}`);
