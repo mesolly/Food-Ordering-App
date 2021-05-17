fcf9a12e1a0eaf6206fd88d065d63e64ca787d6f
mesolly <saurav.kandari.123@gmail.com>
2021-05-17 22:45:07 +0530

    'Add/cartFunctionality'

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,21 @@
-console.log('Hello from appjs') ;
+import axios from 'axios'
+import Noty from 'noty'
+
+let addToCart = document.querySelectorAll('.add-to-cart')
+let cartCounter = document.querySelector('#cartCounter')
+
+function updateCart(food){
+    axios.post('/update-cart',food).then(res =>{
+        cartCounter.innerText = res.data.totalQty
+        new Noty({
+            text: "Notification text"
+        }).show();
+    })
+}
+
+addToCart.forEach((btn)=>{
+    btn.addEventListener('click',(e)=>{
+        let food = JSON.parse(btn.dataset.food)
+        updateCart(food)
+    })
+})
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+require('dotenv').config()
 const express = require('express') 
 const ejs = require('ejs') 
 const expressLayout = require('express-ejs-layouts')
 const path = require('path') 
 const mongoose = require('mongoose')
-
+const session = require('express-session')
+const flash = require('express-flash')
 const app = express() 
+var MongoDbStore = require('connect-mongodb-session')(session);
 
 const PORT=process.env.PORT || 3000 
 
@@ -19,6 +22,30 @@ connection.once('open',()=>{
     console.log('Connection failed ....')
 });
 
+//session store
+
+var mongostore = new MongoDbStore({
+    uri: url,
+    collection: 'sessions'
+    });
+
+//session config
+    app.use(session({
+        secret : process.env.COOKIE_SECRET,
+        resave : false,
+        store : mongostore,
+        saveUninitialized : false,
+        cookie : { maxAge : 1000 * 60 * 60 * 24} //24 hour
+    }))
+//
+app.use(flash())
+//Global middleware
+app.use((req,res,next)=>{
+    res.locals.session = req.session
+    next()
+})
+
+app.use(express.json())
 app.use(expressLayout) 
 app.set('views',path.join(__dirname,'resources/views'))
 app.set('view engine','ejs')
